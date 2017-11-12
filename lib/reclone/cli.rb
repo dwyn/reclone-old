89c61f7e931e0485114af6b0fe22677edaabdca6
dwyn <dwayne.harmon@gmail.com>
2017-11-11 22:02:49 -0600

    Added functionality - Checkng to see if directory exists.

--- lib/reclone/cli.rb
+++ lib/reclone/cli.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 class Reclone::CLI
   def call
     exit unless up?
@@ -6,6 +8,10 @@ class Reclone::CLI
 
   def up?
     Net::Ping::External.new("www.google.com").ping?
+  end
+
+  def file_dir_or_symlink_exists?(path_to_file)
+    File.exist?(path_to_file) || File.symlink?(path_to_file)
   end
 
   def  log_in
@@ -17,9 +23,43 @@ class Reclone::CLI
     user_password = gets.strip
 
     client = Octokit::Client.new(:login => username, :password => user_password )
-    # Fetch the current user
-    client.user
-    binding.pry
+
+    Octokit.auto_paginate = true
+    test = []
+    temp_directory = ""
+
+    client.repositories.each do |repository|
+      
+      temp_directory = Dir.pwd + repository.name
+
+      if file_dir_or_symlink_exists?(temp_directory)
+        puts "The directory #{temp_directory} already exists"
+        puts "...moving on"
+        puts " "
+      else 
+        git clone repository.html_url
+      end 
+      full_name = repository[:full_name]
+      has_push_access = repository[:permissions][:push]
+    
+      access_type = if has_push_access
+          "write"
+        else
+          "read-only"
+        end
+        binding.pry
+
+      puts "User has #{access_type} access to #{full_name}."
+    end
+
+
+
+
+
+
+
+    puts "good bye"
+
   end
 
 end
