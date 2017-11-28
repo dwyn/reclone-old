ab972610202f4fd569eb067b1688be56a55da749
dwyn <dwayne.harmon@gmail.com>
2017-11-27 19:31:16 -0600

    Security updates. Not for the future, but for myself, because I am an idiot. Thank god for God mothers.

--- lib/reclone/cli.rb
+++ lib/reclone/cli.rb
@@ -43,14 +43,19 @@ class Reclone::CLI
 
 # #repositories(user = nil, options = {}) ⇒ Array<Sawyer::Resource>
 # Also known as: list_repositories, list_repos, repos
+# system "sudo apt-get -y install vim"
 
+# if $?.exitstatus > 0
+#   puts "I failed to install Vim, I am very sorry :'(" 
+# end
+# Executes cmd in a subshell, returning true if the command was found and ran successfully, false otherwise. An error status is available in $?. The arguments are processed in the same way as for Kernel::exec.
   
   def recloner
     ratelimit           = Octokit.ratelimit
     ratelimit_remaining = Octokit.rate_limit.remaining
     puts "Rate Limit Remaining: #{ratelimit_remaining} / #{ratelimit}"
     puts
-    
+    #SYSTEM METHOS!
     # temp_directory = ""
     repos = Octokit.repositories("dwyn", {sort: :pushed_at})
     binding.pry
@@ -89,6 +94,67 @@ class Reclone::CLI
 end
 
 
+
+
+########################
+# You don't ever want to commit code with passwords or access tokens (any sensitive data). Typically you grab credentials from environment variables or because this is ruby you could make a yaml file and load your credentials that way. Something like...
+
+# require 'yaml'
+# require 'spotify-client'
+
+# class Beats
+#   def initialize(config)
+#     secret = YAML.load_file(config)
+#     creditials = get_config(secret)
+#     @spotify = Spotify::Client.new(creditials)
+#     ids = track_ids(get_user_playlist)
+#     get_song_attributes(ids)
+#   end
+
+#   def get_config(config_hash)
+#     config = {
+#       :access_token => config_hash['ACCESS_TOKEN'],
+#       :client_secret => config_hash['CLIENT_SECRET']
+#     }
+#   end
+
+#   ...
+# end
+# Where your yaml file would look like:
+
+# ACCESS_TOKEN: 'my-access-token'
+# CLIENT_SECRET: 'my-client-secret'
+
+##########################
+# require 'octokit'
+
+# # API 呼び出し回数
+# ratelimit           = Octokit.ratelimit
+# ratelimit_remaining = Octokit.ratelimit_remaining
+# puts "Rate Limit Remaining: #{ratelimit_remaining} / #{ratelimit}"
+# puts
+
+# # インスタンス化
+# # ==== 認証無しの場合
+# repos = Octokit.repositories("komasaru", {sort: :pushed_at})
+# # ==== OAuth 認証の場合
+# #cl = Octokit::Client.new(login: "komasaru", oauth_token: "token_string")
+# #repos = cl.repositories("komasaru", {sort: :pushed_at})
+
+# # 値取得
+# repos.each do |repo|
+#   puts "[ #{repo.name} ]"
+#   puts "\tOwner       : #{repo.owner.login}"
+#   puts "\tFull Name   : #{repo.full_name}"
+#   puts "\tDescription : #{repo.description}"
+#   puts "\tPrivate     : #{repo.private}"
+#   puts "\tLanguage    : #{repo.language}"
+#   puts "\tURL         : #{repo.html_url}"
+#   puts "\tCreated at  : #{repo.created_at}"
+#   puts "\tUpdated at  : #{repo.updated_at}"
+#   puts "\tPushed  at  : #{repo.pushed_at}"
+#   puts
+# end
 
 
 # def clone(repository, name, opts = {})
